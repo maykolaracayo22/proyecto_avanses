fa8e46960b0c959b42d376459857ee6fc8737714
EdyTola <jaircitola@gmail.com>
2024-10-30 11:20:58 -0500

    first commit

---
+++ asistencia_upeu/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:asistencia_upeu/theme/AppTheme.dart';
+import 'package:flutter/material.dart';
+
+import 'login/login_google.dart';
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "UPeU",
+      theme: AppTheme.themeData,
+      debugShowCheckedModeBanner: false,
+      home: MainLogin(),
+    );
+  }
+}
